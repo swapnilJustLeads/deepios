482b99bdc698d365952b402133179b52b2a1b66b
DroidBauer <shubham@justgetleads.com>
2025-03-09 19:19:30 +0530

    theme update hook

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import { lightColors, darkColors } from '../theme/colors';
+
+export const useTheme = () => {
+  const isDarkMode = useSelector((state) => state.theme.darkMode);
+  
+  return {
+    colors: isDarkMode ? darkColors : lightColors,
+    isDarkMode,
+  };
+};
